c69fd144f00e9c9bd568303410ad640af9e51feb
davidhenshaw <davidhenshaw2014@gmail.com>
2021-04-14 15:07:15 -0500

    Add function to create toy cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const url = 'http://localhost:3000/toys';
+
+fetch(url)
+.then(response => response.json())
+.then(json => console.log(json));
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +18,36 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  let div = makeCard("pink", '', 7, 3);
+  document.querySelector("#toy-collection").appendChild(div);
 });
+
+function makeCard(name, imgSource, likes, toyId)
+{
+  let div = document.createElement('div');
+  div.classList = ['card'];
+
+  let h2 = document.createElement('h2');
+  h2.innerText = name;
+  div.appendChild(h2);
+
+  let img = document.createElement('img');
+  img.src = imgSource;
+  img.classList = 'toy-avatar';
+  div.appendChild(img);
+
+  let p = document.createElement('p');
+  p.innerText = `${likes} Likes`;
+  div.appendChild(p);
+
+  let button = document.createElement('button');
+  button.id = toyId;
+  button.classList = ['like-btn'];
+  button.innerText = 'Like <3';
+  div.appendChild(button);
+
+  return div;
+}
+
+
